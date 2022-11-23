38747b1b1d9ee34e07544310174b442da8945cfa
Estivenph <111470175+Estivenph@users.noreply.github.com>
2022-11-23 18:18:41 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ app.get('/', async (req, res) => {
   
   app.post('/clientes', async (req, res) => {
     const data = req.body
+    console.log(data);
     const [rows] = await pool.query('insert into cliente (documento, nombre ,apellidos ,direccion) select data.documento, data.nombre, data.apellidos, data.direccion')
 	res.json(rows[0][0])
   })
